96a56e2094ef1233c0c73caaab5d530c7507df3f
unknown <rakibulanas033@gmail.com>
2022-07-13 14:00:24 +0600

    first commit

---
+++ read-post-server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wrj79b8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+	try {
+		await client.connect();
+		const posts = client.db("Blog").collection("posts");
+
+		app.get("/", async (req, res) => {
+			const query = {};
+			const cursor = posts.find(query);
+			const allPost = await cursor.toArray();
+			res.send(allPost);
+		});
+	} finally {
+		//await client.close();
+	}
+}
+run().catch(console.dir);
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
