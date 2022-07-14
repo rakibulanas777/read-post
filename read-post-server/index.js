28ac03b660c48849766bc8e1662bb6e5213b62fc
unknown <rakibulanas033@gmail.com>
2022-07-14 20:03:38 +0600

    final mern curd

--- read-post-server/index.js
+++ read-post-server/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require("dotenv").config();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+// use middleware
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wrj79b8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -19,22 +21,26 @@ async function run() {
 	try {
 		await client.connect();
 		const posts = client.db("Blog").collection("posts");
-
-		app.get("/", async (req, res) => {
+		//read full post
+		app.get("/posts", async (req, res) => {
 			const query = {};
 			const cursor = posts.find(query);
 			const allPost = await cursor.toArray();
 			res.send(allPost);
 		});
-		app.get("/:id", async (req, res) => {
+		// read single post
+		app.get("/posts/:id", async (req, res) => {
 			const id = req.params.id;
-			const query = { _id: ObjectId(id) };
+
+			const query = {
+				_id: ObjectId(id),
+			};
 			const post = await posts.findOne(query);
 			res.send(post);
 		});
 
 		//update post
-		app.put("/:id", async (req, res) => {
+		app.put("/posts/:id", async (req, res) => {
 			const id = req.params.id;
 			console.log(id);
 			const editPost = req.body;
@@ -49,11 +55,22 @@ async function run() {
 			const result = await posts.updateOne(filter, updateDoc, options);
 			res.send(result);
 		});
+
+		//delete post
+		app.delete("/posts/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const result = await posts.deleteOne(query);
+			res.send(result);
+		});
 	} finally {
 		//await client.close();
 	}
 }
 run().catch(console.dir);
+app.get("/", (req, res) => {
+	res.send("Hello world");
+});
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
